42c4a4244ab63ae83c54717f7b5d06ec4cad09a2
Victor Manuel Rodriguez <victormanuelrodriguez90@gmail.com>
2016-02-26 11:11:28 -0600

    Added files via upload

---
+++ bonusChallengeEChallenge4.js
@@ -0,0 +1,16 @@
+function FibonacciSerie(){
+    var prev = 0, next = 1 ;
+    var sumEven = [];
+    do{
+        prev = prev + next;
+        next = prev + next;
+        if(prev%2==0) sumEven.push(prev);
+        if(next%2==0) sumEven.push(next);
+    }
+    while(pre+curr > 4000000)
+    return sumEven.reduce(function(a, b) {
+        return a + b;
+    });
+}
+
+
